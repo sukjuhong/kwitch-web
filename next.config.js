2b94066ce553966823dda033ae6a50bab00b124d
Sukju Hong <ehensnfl@gmail.com>
2024-02-08 14:56:25 +0900

    feat: add API_URL for production

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const API_URL = process.env.API_SERVER_URL || "http://localhost:8000";
+
 const nextConfig = {
   rewrites: async () => [
     {
       source: "/api/:path*",
-      destination: "http://localhost:8000/api/:path*",
+      destination: `${API_URL}/api/:path*`,
     },
     {
       source: "/socket.io",
-      destination: "http://localhost:8000/socket.io/",
+      destination: `${API_URL}/socket.io/`,
     },
   ],
   reactStrictMode: false,
